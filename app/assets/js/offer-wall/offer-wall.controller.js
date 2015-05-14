a5e7800042df01a87bfb3693a7a46aa6b4f40f98
Wagner Camarao <wagner@camarao.io>
2015-05-13 22:28:54 -0700

    Paginate through offer results

--- app/assets/js/offer-wall/offer-wall.controller.js
+++ app/assets/js/offer-wall/offer-wall.controller.js
@@ -5,11 +5,37 @@
     var ctrl = this;
     ctrl.searchModel = { uid: 'player1', page: 1 };
     ctrl.isLoading = false;
-    ctrl.search = search;
     search();
 
     ctrl.hasOffers = function() {
       return !!ctrl.totalOffersCount && !!ctrl.offers;
+    };
+
+    ctrl.hasPrev = function() {
+      return ctrl.searchModel.page > 1;
+    };
+
+    ctrl.hasNext = function() {
+      return ctrl.searchModel.page < ctrl.totalPages;
+    };
+
+    ctrl.prev = function() {
+      ctrl.searchModel.page--;
+      search();
+    };
+
+    ctrl.next = function() {
+      ctrl.searchModel.page++;
+      search();
+    };
+
+    ctrl.search = function() {
+      ctrl.searchModel.page = 1;
+      search();
+    };
+
+    ctrl.retry = function() {
+      search();
     };
 
     function search() {
@@ -18,12 +44,16 @@
 
       request.success(function(response) {
         ctrl.offers = response.offers;
+        ctrl.totalPages = response.pages;
         ctrl.totalOffersCount = response.count;
+        ctrl.message = response.message;
       })
 
       request.error(function() {
         ctrl.offers = null;
+        ctrl.totalPages = null;
         ctrl.totalOffersCount = null;
+        ctrl.message = 'Something went wrong. Please try again later.';
       })
 
       request.finally(function() {
